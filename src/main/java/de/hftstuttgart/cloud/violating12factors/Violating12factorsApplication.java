73e4f63cea45bef6cc0addec62ff7f5f204d6fc3
speiser <sebastian.speiser@hft-stuttgart.de>
2021-04-30 14:37:23 +0200

    initial import

---
+++ src/main/java/de/hftstuttgart/cloud/violating12factors/Violating12factorsApplication.java
@@ -0,0 +1,13 @@
+package de.hftstuttgart.cloud.violating12factors;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Violating12factorsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Violating12factorsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/hftstuttgart/cloud/violating12factors/Violating12factorsApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hftstuttgart.cloud.violating12factors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Violating12factorsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
